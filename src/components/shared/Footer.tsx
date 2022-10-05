dd33645871961a2b1a5247cf166d97d67769e37a
Adrian W <107289257+tyk-adrian@users.noreply.github.com>
2022-10-05 13:34:18 +0800

    add dmca link in footer (#4)

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,8 +1,12 @@
 import styled from '@emotion/styled'
 import React from 'react'
 
+import content from '../../content/default.yaml'
+
 export const Container = styled.footer<Props>`
+  padding: ${({ theme }) => theme.spacing(8, 0)};
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background-color: ${({ isLight, theme }) =>
@@ -10,11 +14,19 @@ export const Container = styled.footer<Props>`
   color: ${({ isLight, theme }) => theme.colors.common[isLight ? 'black' : 'white']};
 `
 
-export const Link = styled.a`
-  padding: ${({ theme }) => theme.spacing(8, 0)};
+export const TaiyakiLink = styled.a`
   font-weight: 300;
   text-transform: uppercase;
   letter-spacing: 4px;
+  font-size: 0.75rem;
+  color: ${({ theme }) => theme.colors.common.white};
+  text-decoration: none;
+`
+
+export const Link = styled.a`
+  opacity: 0.7;
+  margin-top: ${({ theme }) => theme.spacing(2)};
+  font-weight: 300;
   font-size: 0.75rem;
   color: ${({ theme }) => theme.colors.common.white};
   text-decoration: none;
@@ -27,8 +39,15 @@ interface Props {
 export function Footer({ isLight = true }: Props) {
   return (
     <Container isLight={isLight}>
-      <Link href="https://taiyakistudios.com" target="_blank" rel="noopener noreferrer">
+      <TaiyakiLink
+        href="https://taiyakistudios.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         © Taiyaki Studios
+      </TaiyakiLink>
+      <Link href={content.dmca_policy_link} target="_blank" rel="noopener noreferrer">
+        DMCA Policy
       </Link>
     </Container>
   )
